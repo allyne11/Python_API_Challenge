3987db7e3dfbd63bb9ce6fa3b962127917ded935
allyne11 <aidanlyne221@gmail.com>
2025-03-03 21:15:33 -0600

    Done

--- Starter_Code/api_keys.py
+++ Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "25f20e6aaa2fca6434e3727ceaf4c8d9"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "486fbdf0e14a46f6b208a317e7467555"
